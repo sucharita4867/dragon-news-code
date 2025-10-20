5c403477f4c9d8d774e8021fff3da0c45879076b
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-20 12:33:10 +0530

    stader news details page create

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import Header from "../Components/Header";
+import RightAside from "../Components/HomeLayout/RightAside";
 
 const NewsDetails = () => {
-      return (
-            <div>
-                  newsDetails page
-            </div>
-      );
+  return (
+    <div>
+      <header className="py-3">
+        <Header></Header>
+      </header>
+      <main className="w-11/12 mx-auto grid grid-cols-12 gap-5 py-10">
+        <section className="col-span-9">
+          <h1 className="font-bold bg-4">News Details</h1>
+        </section>
+        <aside className="col-span-3">
+          <RightAside></RightAside>
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default NewsDetails;
