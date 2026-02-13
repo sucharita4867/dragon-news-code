f9c9befafff6d376c1bfa0f9fbb92321a1364c55
sucharita4867 <sucharitasardar7@gmail.com>
2026-02-13 23:03:45 +0530

    delete error message in login components

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -21,7 +21,6 @@ const Login = () => {
       })
       .catch((error) => {
         const errorCode = error.code;
-        // const errorMessage = error.message;
         // alert(errorCode, errorMessage);
         setError(errorCode);
       });
